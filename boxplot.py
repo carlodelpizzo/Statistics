71e481492e0005c44a73d8414a21f640dfab6a38
Bones Malin <carlodelpizzo@gmail.com>
2020-08-26 14:42:39 -0400

    For Stat Homework

--- boxplot.py
+++ boxplot.py
@@ -1,19 +1,37 @@
 import matplotlib.pyplot as plt
+from quantiles import *
+
+# Compute the median and the first and third quartiles of the sample.
+# Indicate these with horizontal lines.
+# Draw vertical lines to complete the box.
+# Find the largest sample value that is no more than 1.5 IQR above the third quartile,
+# and the smallest sample value that is no more than 1.5 IQR below the first quartile.
+# Extend vertical lines (whiskers) from the quartile lines to these points.
+# Points more than 1.5 IQR above the third quartile, or more than 1.5 IQR below the first quartile,
+# are designated as outliers. Plot each outlier individually.
+
 
 cat_a = [4.4, 3.4, 2.6, 3.8, 4.9, 4.6, 5.2, 4.7, 4.1, 2.6, 6.7, 4.1, 3.6, 2.9,
          2.6, 4.0, 4.3, 3.9, 4.8, 4.5, 4.4, 3.1, 5.7, 4.5]
 cat_a.sort()
+cat_a_median = quantile_val(cat_a, 2)
+cat_a_quartiles = quantile_val(cat_a, 4)
+cat_a_first_quart = cat_a_quartiles[0]
+cat_a_third_quart = cat_a_quartiles[2]
+cat_a_IQR = cat_a_third_quart - cat_a_first_quart
+
 
 cat_b = [3.4, 1.1, 2.9, 5.5, 6.4, 5.0, 5.8, 2.5, 3.7, 3.8, 3.1, 1.6, 3.5, 5.9,
          6.7, 5.2, 6.3, 2.6, 4.3, 3.8]
 cat_b.sort()
-
+cat_b_median = quantile_val(cat_b, 2)
+cat_b_quartiles = quantile_val(cat_b, 4)
+cat_b_first_quart = cat_b_quartiles[0]
+cat_b_third_quart = cat_b_quartiles[2]
+cat_b_IQR = cat_b_third_quart - cat_b_first_quart
 
 plt.boxplot((cat_a, cat_b), sym='x', whis=1.5, positions=[1, 2], widths=0.8,
             labels=('Catalyst A', 'Catalyst B'))
 plt.title('')
 plt.ylabel('Percentage Yield')
-
-print(cat_a)
-print(cat_b)
 plt.show()
--- quantiles.py
+++ quantiles.py
@@ -1,26 +1,63 @@
-data = [2, 18, 23, 41, 44, 46, 49, 61, 62, 74, 76, 79, 82, 89, 92, 95]
-split = 1/5
+def quantile_val(array, k):
+    if k > len(array):
+        k = len(array)
 
-if 1/split > len(data):
-    split = 1/len(data)
+    val = []
+    for i in range(1, k):
+        div = (i / k) * (len(array) + 1)
+        if div % 1 != 0:
+            split_1 = int(div) + 1
+            div = int(div)
+            value = (array[div - 1] + array[split_1 - 1]) / 2
+            if value % 1 == 0:
+                value = int(value)
+            val.append(value)
+        else:
+            val.append(array[int(div) - 1])
 
-k = int(1/split)
-split_points = []
-split_values = []
-for i in range(1, k):
-    split = (i/k)*(len(data) + 1)
-    if split % 1 != 0:
-        split_1 = int(split) + 1
-        split = int(split)
-        split_points.append((split, split_1))
-        value = (data[split - 1] + data[split_1 - 1]) / 2
-        if value % 1 == 0:
-            value = int(value)
-        split_values.append(value)
-    else:
-        split_points.append(int(split))
-        split_values.append(data[int(split) - 1])
+    return val
 
 
-print('The split points are: {}'.format(split_points))
-print('The split values are: {}'.format(split_values))
+def quantile_index(array, k):
+    if k > len(array):
+        k = len(array)
+
+    index = []
+    for i in range(1, k):
+        div = (i / k) * (len(array) + 1)
+        if div % 1 != 0:
+            split_1 = int(div) + 1
+            div = int(div)
+            index.append((div, split_1))
+        else:
+            index.append(int(div))
+
+    return index
+
+
+# data = [2, 18, 23, 41, 44, 46, 49, 61, 62, 74, 76, 79, 82, 89, 92, 95]
+# split = 1/5
+#
+# if 1/split > len(data):
+#     split = 1/len(data)
+#
+# k = int(1/split)
+# split_points = []
+# split_values = []
+# for i in range(1, k):
+#     split = (i/k)*(len(data) + 1)
+#     if split % 1 != 0:
+#         split_1 = int(split) + 1
+#         split = int(split)
+#         split_points.append((split, split_1))
+#         value = (data[split - 1] + data[split_1 - 1]) / 2
+#         if value % 1 == 0:
+#             value = int(value)
+#         split_values.append(value)
+#     else:
+#         split_points.append(int(split))
+#         split_values.append(data[int(split) - 1])
+#
+#
+# print('The split points are: {}'.format(split_points))
+# print('The split values are: {}'.format(split_values))
